bc817babf7d314446f8c446096e7272512b26b77
Oldwolf <bypang1226@oldwolf-server.(none)>
2012-06-29 21:20:11 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wolfpack::Application.config.secret_token = '18574113a4c0c3849234ca56ab0d481fb29b9b7e90092b54b44bb601072c65a29de6eca780068ab88ee149cd39c08d7d4a1ecc13e80457d07e35452322570e5a'
